4cd20394c1f016077e00801719ef13dfa91fb760
Varshith BR <111895368+varshithgitub@users.noreply.github.com>
2024-03-15 17:27:05 -0400

    r1.py

--- q1.py
+++ q1.py
@@ -1,8 +1,10 @@
 #1 
+
+#this is the change
 def val(ip_list):
     return [ i for i in ip_list if i%2==0]
 
 ip = input("enter the no. in a sequence")
 
 ip_list = list(map(int, ip.split()))
-print("even numbers are",val(ip_list))
+print("even numbers are",val(ip_lis))
