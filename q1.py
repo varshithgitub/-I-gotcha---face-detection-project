ac83bfe9e157ec1e79588a40a914271e60cb5984
Varshith BR <111895368+varshithgitub@users.noreply.github.com>
2024-03-15 17:25:11 -0400

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,8 @@
+#1 
+def val(ip_list):
+    return [ i for i in ip_list if i%2==0]
+
+ip = input("enter the no. in a sequence")
+
+ip_list = list(map(int, ip.split()))
+print("even numbers are",val(ip_list))
